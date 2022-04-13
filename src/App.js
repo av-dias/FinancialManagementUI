ee49e88693702b9a9acb92898609ada849d3a422
av.dias <av.dias@campus.fct.unl.pt>
2022-04-13 12:25:00 +0100

    final ui demo template

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
-import React from "react";
+import React from 'react';
 
-import { Article, Brand, Cta, Feature, Navbar } from "./components";
-import {
-  Footer,
-  Blog,
-  Possibility,
-  Features,
-  WhatGPT3,
-  Header,
-} from "./containers";
-import "./App.css";
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="gradient__bg">
-        <Navbar />
-        <Header />
-      </div>
-      <Brand />
-      <WhatGPT3 />
-      <Features />
-      <Possibility />
-      <Cta />
-      <Blog />
-      <Footer />
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  );
-}
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+import App from './App';
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
