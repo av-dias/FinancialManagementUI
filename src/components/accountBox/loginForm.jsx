d059433f6eb4cfceb12335d3a77a96a9324e344c
av.dias <av.dias@campus.fct.unl.pt>
2022-04-19 22:24:03 +0100

    adding login form and understanding codebase logic

---
+++ src/components/accountBox/loginForm.jsx
@@ -0,0 +1,37 @@
+import React, { useContext } from "react";
+import {
+  BoldLink,
+  BoxContainer,
+  FormContainer,
+  Input,
+  MutedLink,
+  SubmitButton,
+} from "./common";
+import { Marginer } from "../marginer";
+import { AccountContext } from "./accountContext";
+
+export function LoginForm() {
+  const { switchToSignup, switchToForgotPassword } = useContext(AccountContext);
+
+  return (
+    <BoxContainer>
+      <FormContainer>
+        <Input type="email" placeholder="Email" />
+        <Input type="password" placeholder="Password" />
+      </FormContainer>
+      <Marginer direction="vertical" margin={10} />
+      <MutedLink href="#" onClick={switchToForgotPassword}>
+        Forget your password?
+      </MutedLink>
+      <Marginer direction="vertical" margin="1.6em" />
+      <SubmitButton type="submit">Signin</SubmitButton>
+      <Marginer direction="vertical" margin="1em" />
+      <MutedLink>
+        Do not have an accoun?
+        <BoldLink href="#" onClick={switchToSignup}>
+          Signup
+        </BoldLink>
+      </MutedLink>
+    </BoxContainer>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+/*!
 
-import App from './App';
-import './index.css';
+=========================================================
+* Material Dashboard React - v1.10.0
+=========================================================
 
-ReactDOM.render(<App />, document.getElementById('root'));
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+// core components
+import Admin from "layouts/Admin.js";
+import RTL from "layouts/RTL.js";
+import Home from "layouts/Home.js";
+
+import "assets/css/material-dashboard-react.css?v=1.10.0";
+import "assets/css/index.css";
+import "assets/css/App.css";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/admin" component={Admin} />
+      <Route path="/rtl" component={RTL} />
+      <Route path="/" component={Home} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
